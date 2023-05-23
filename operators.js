02d066304ee6cc1bc658ac69933071a331a988ad
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-05-23 16:44:32 -0600

    12

--- operators.js
+++ operators.js
@@ -30,7 +30,23 @@ function encounter() {
     }
   }
 }
-
-function idHint(){
-    // Should return true if a randomly generated number is greater
+function shipCanFire() {
+  // return true if the ships health is above 0 AND ammo is above 0 false otherwise
+  return shipAmmo > 0 && shipHealth > 0;
 }
+function reloadShip() {
+  // reduce ship health by 1 and increase ammo by 3
+  shipHealth--;
+  shipAmmo += 3;
+}
+function isDestroyed(health) {
+  // return true if health is zero or less
+  return health <= 0;
+}
+function isHit() {
+  // Should return true if a randomly generated number is greater
+  return Math.random() > 0.5;
+}
+
+encounter();
+
